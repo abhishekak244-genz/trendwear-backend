f2de65d764b3d04975b9d672bb5e2cd1130a244b
Abhishek AK <abhishekak244@gmail.com>
2026-06-03 19:39:16 +0530

    Fix final module import

--- seeder.js
+++ seeder.js
@@ -1,7 +1,8 @@
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const Product = require("./models/Product");
-const User = require("./models/User");
+const User = require("./models/user");
+
 const cart = require("./models/cart");
 const products = require("./data/products");
 
